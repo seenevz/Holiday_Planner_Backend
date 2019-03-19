731876f97a2afbf8ace46cb43568f94e78ec7ff9
seenevz <serneves@hotmail.com>
2019-03-19 15:14:32 +0000

    added api integration

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'graphql'
 gem 'search_object'
 gem 'search_object_graphql'
 
+# REST Client
+gem 'rest-client'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/models/API.rb
@@ -0,0 +1,21 @@
+# account = 'YHLCM4NM'
+# key = '38erzyxay2eh4ieiwg0wx0xpuyi5yhah'
+require 'rest-client'
+
+class API
+   
+   def self.get_request (endpoint, query)
+      url = "https://www.triposo.com/api/20181213/#{endpoint}.json?#{query}"
+      headers = {
+         'X-Triposo-Account': ENV['API_USER'],
+         'X-Triposo-Token': ENV['API_KEY']
+      }
+
+      response = RestClient.get(
+         url,
+         headers
+      )
+      
+      [response.code, JSON.parse(response.body)]
+   end
+end
